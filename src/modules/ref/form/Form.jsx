5564d5f6ebed42413fc1e436a74f914630409685
algomachine007 <amaben202phantom@gmail.com>
2024-03-16 19:34:01 +0100

    creating useIm perativeHandle

--- src/modules/ref/form/Form.jsx
+++ src/modules/ref/form/Form.jsx
@@ -1,5 +1,26 @@
+import { useRef, useState } from "react";
+import InputWithUseImperativeHandle from "./Input";
+
 const FormWithUseImperativeHandle = () => {
-  return <div>FormWithUseImperativeHandle</div>;
+  const [name, setName] = useState("");
+  const inputRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      inputRef.current.focus();
+      inputRef.current.shake();
+    } else {
+      alert("Sent");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">FormWithUseImperativeHandle</label>
+
+      <InputWithUseImperativeHandle parentRef={inputRef} setName={setName} />
+    </form>
+  );
 };
 
 export default FormWithUseImperativeHandle;
--- src/modules/ref/form/Input.jsx
+++ src/modules/ref/form/Input.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import clsx from "clsx";
 import { useImperativeHandle, useRef, useState } from "react";
-const InputWithUseImperativeHandle = ({ parentRef }) => {
+const InputWithUseImperativeHandle = ({ parentRef, setName }) => {
   const [shakeInput, setShakeInput] = useState(false);
   const ref = useRef(null);
 
@@ -16,6 +16,7 @@ const InputWithUseImperativeHandle = ({ parentRef }) => {
 
   return (
     <input
+      onChange={(e) => setName(e.target.value)}
       className={clsx(shakeInput && "animate-shake", "p-3 rounded-xl border")}
     />
   );
